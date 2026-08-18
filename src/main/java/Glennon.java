9ad524295e057f66a63c9bcc3e1f971bf8088da9
Zhu Zhi Yu <zhuzhiyu04@gmail.com>
2026-08-18 17:59:17 +0800

    Add ASCII banner for Glennon
    Display a boxed Glennon banner at startup and document the application entry point with Javadoc.

--- src/main/java/Glennon.java
+++ src/main/java/Glennon.java
@@ -2,7 +2,21 @@
  * Starts the Glennon chatbot.
  */
 public class Glennon {
+    /**
+     * Displays the Glennon chatbot banner when the application starts.
+     *
+     * @param args command-line arguments; currently unused
+     */
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Glennon.");
+        String banner = """
+                +-----------------------------------------+
+                |   ____ _                                |
+                |  / ___| | ___ _ __  _ __   ___  _ __    |
+                | | |  _| |/ _ \\ '_ \\| '_ \\ / _ \\| '_ \\   |
+                | | |_| | |  __/ | | | | | | | (_) | | | ||
+                |  \\____|_|\\___|_| |_|_| |_|\\___/|_| |_|  |
+                +-----------------------------------------+
+                """;
+        System.out.print(banner);
     }
 }
