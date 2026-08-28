037d5ca2904e5a86034e0940dbc3733f8b223e5b
Zhu Zhi Yu <zhuzhiyu04@gmail.com>
2026-08-29 02:22:28 +0800

    Encapsulate command and status handling
    Delegate individual command execution from the application loop and
    move indexed mark and unmark operations into TaskList so list
    validation stays with list state.

--- src/main/java/glennon/Glennon.java
+++ src/main/java/glennon/Glennon.java
@@ -50,44 +50,7 @@ public class Glennon {
             ui.showDivider();
 
             try {
-                Parser.CommandType commandType = Parser.parseCommandType(command);
-
-                switch (commandType) {
-                case BYE -> {
-                    ui.showGoodbye();
-                    isSigningOff = true;
-                }
-                case LIST -> ui.showMissionList(missions.asList());
-                case ON -> {
-                    var date = Parser.parseDate(command);
-                    ui.showScheduledMissions(date, missions.occurringOn(date));
-                }
-                case DELETE -> {
-                    int missionIndex = Parser.parseMissionIndex(command, commandType);
-                    Task removedMission = missions.remove(missionIndex);
-                    storage.saveMissions(missions.asList());
-                    ui.showMissionRemoved(removedMission, missions.size());
-                }
-                case MARK, UNMARK -> {
-                    boolean shouldCompleteMission = commandType == Parser.CommandType.MARK;
-                    int missionIndex = Parser.parseMissionIndex(command, commandType);
-                    Task mission = missions.get(missionIndex);
-                    if (shouldCompleteMission) {
-                        mission.markAsDone();
-                    } else {
-                        mission.markAsNotDone();
-                    }
-                    storage.saveMissions(missions.asList());
-                    ui.showMissionStatusChanged(mission, shouldCompleteMission);
-                }
-                case TODO -> addMission(Parser.parseTodo(command));
-                case DEADLINE -> addMission(Parser.parseDeadline(command));
-                case EVENT -> addMission(Parser.parseEvent(command));
-                case UNKNOWN -> throw new GlennonException(
-                        "Glennon doesn't recognize that command.\n"
-                                + "Try: todo, deadline, event, list, on, mark, unmark, "
-                                + "delete, or bye.");
-                }
+                isSigningOff = executeCommand(command);
             } catch (GlennonException e) {
                 ui.showError(e.getMessage());
             }
@@ -104,6 +67,63 @@ public class Glennon {
      */
     public static void main(String[] args) {
         new Glennon("data/glennon.txt").run();
+    }
+
+    /**
+     * Executes one user command and reports whether Glennon should sign off.
+     *
+     * @param command complete command entered by the user
+     * @return true when the command is {@code bye}, otherwise false
+     * @throws GlennonException if the command cannot be completed
+     */
+    private boolean executeCommand(String command) throws GlennonException {
+        Parser.CommandType commandType = Parser.parseCommandType(command);
+
+        switch (commandType) {
+        case BYE -> {
+            ui.showGoodbye();
+            return true;
+        }
+        case LIST -> ui.showMissionList(missions.asList());
+        case ON -> {
+            var date = Parser.parseDate(command);
+            ui.showScheduledMissions(date, missions.occurringOn(date));
+        }
+        case DELETE -> {
+            int missionIndex = Parser.parseMissionIndex(command, commandType);
+            Task removedMission = missions.remove(missionIndex);
+            storage.saveMissions(missions.asList());
+            ui.showMissionRemoved(removedMission, missions.size());
+        }
+        case MARK, UNMARK -> updateMissionStatus(command, commandType);
+        case TODO -> addMission(Parser.parseTodo(command));
+        case DEADLINE -> addMission(Parser.parseDeadline(command));
+        case EVENT -> addMission(Parser.parseEvent(command));
+        case UNKNOWN -> throw new GlennonException(
+                "Glennon doesn't recognize that command.\n"
+                        + "Try: todo, deadline, event, list, on, mark, unmark, "
+                        + "delete, or bye.");
+        }
+        return false;
+    }
+
+    /**
+     * Marks or unmarks the mission identified by a command and saves the
+     * resulting mission list.
+     *
+     * @param command complete mark or unmark command
+     * @param commandType type of status-changing command
+     * @throws GlennonException if the mission number or save operation fails
+     */
+    private void updateMissionStatus(
+            String command, Parser.CommandType commandType) throws GlennonException {
+        boolean shouldCompleteMission = commandType == Parser.CommandType.MARK;
+        int missionIndex = Parser.parseMissionIndex(command, commandType);
+        Task mission = shouldCompleteMission
+                ? missions.mark(missionIndex)
+                : missions.unmark(missionIndex);
+        storage.saveMissions(missions.asList());
+        ui.showMissionStatusChanged(mission, shouldCompleteMission);
     }
 
     /**
