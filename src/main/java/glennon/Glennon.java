74b63cb088303d00a35050d1643f975a8b8f770d
Zhu Zhi Yu <zhuzhiyu04@gmail.com>
2026-08-29 02:31:46 +0800

    Execute parsed command objects
    Have Parser construct concrete commands with validated arguments and
    simplify Glennon's run loop to execute commands and inspect their exit
    state.

--- src/main/java/glennon/Glennon.java
+++ src/main/java/glennon/Glennon.java
@@ -1,7 +1,7 @@
 package glennon;
 
+import glennon.command.Command;
 import glennon.exception.GlennonException;
-import glennon.task.Task;
 
 import java.nio.file.Path;
 
@@ -50,11 +50,14 @@ public class Glennon {
             ui.showDivider();
 
             try {
-                isSigningOff = executeCommand(command);
+                Command parsedCommand = Parser.parse(command);
+                parsedCommand.execute(missions, ui, storage);
+                isSigningOff = parsedCommand.isExit();
             } catch (GlennonException e) {
                 ui.showError(e.getMessage());
+            } finally {
+                ui.showDivider();
             }
-            ui.showDivider();
         }
 
         ui.close();
@@ -67,75 +70,6 @@ public class Glennon {
      */
     public static void main(String[] args) {
         new Glennon("data/glennon.txt").run();
-    }
-
-    /**
-     * Executes one user command and reports whether Glennon should sign off.
-     *
-     * @param command complete command entered by the user
-     * @return true when the command is {@code bye}, otherwise false
-     * @throws GlennonException if the command cannot be completed
-     */
-    private boolean executeCommand(String command) throws GlennonException {
-        Parser.CommandType commandType = Parser.parseCommandType(command);
-
-        switch (commandType) {
-        case BYE -> {
-            ui.showGoodbye();
-            return true;
-        }
-        case LIST -> ui.showMissionList(missions.asList());
-        case ON -> {
-            var date = Parser.parseDate(command);
-            ui.showScheduledMissions(date, missions.occurringOn(date));
-        }
-        case DELETE -> {
-            int missionIndex = Parser.parseMissionIndex(command, commandType);
-            Task removedMission = missions.remove(missionIndex);
-            storage.saveMissions(missions.asList());
-            ui.showMissionRemoved(removedMission, missions.size());
-        }
-        case MARK, UNMARK -> updateMissionStatus(command, commandType);
-        case TODO -> addMission(Parser.parseTodo(command));
-        case DEADLINE -> addMission(Parser.parseDeadline(command));
-        case EVENT -> addMission(Parser.parseEvent(command));
-        case UNKNOWN -> throw new GlennonException(
-                "Glennon doesn't recognize that command.\n"
-                        + "Try: todo, deadline, event, list, on, mark, unmark, "
-                        + "delete, or bye.");
-        }
-        return false;
-    }
-
-    /**
-     * Marks or unmarks the mission identified by a command and saves the
-     * resulting mission list.
-     *
-     * @param command complete mark or unmark command
-     * @param commandType type of status-changing command
-     * @throws GlennonException if the mission number or save operation fails
-     */
-    private void updateMissionStatus(
-            String command, Parser.CommandType commandType) throws GlennonException {
-        boolean shouldCompleteMission = commandType == Parser.CommandType.MARK;
-        int missionIndex = Parser.parseMissionIndex(command, commandType);
-        Task mission = shouldCompleteMission
-                ? missions.mark(missionIndex)
-                : missions.unmark(missionIndex);
-        storage.saveMissions(missions.asList());
-        ui.showMissionStatusChanged(mission, shouldCompleteMission);
-    }
-
-    /**
-     * Adds a typed mission and displays its type-specific representation.
-     *
-     * @param mission mission to add
-     * @throws GlennonException if the updated mission list cannot be saved
-     */
-    private void addMission(Task mission) throws GlennonException {
-        missions.add(mission);
-        storage.saveMissions(missions.asList());
-        ui.showMissionAdded(mission, missions.size());
     }
 
 }
